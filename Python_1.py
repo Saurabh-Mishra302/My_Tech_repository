3c3409ad4da4c6d7404c7006d9d5e21e31b5e3a0
Saurabh-Mishra302 <saumishra@levi.com>
2024-12-03 00:59:09 +0530

    Initial Commit

---
+++ Python_1.py
@@ -0,0 +1,5 @@
+print("Hello World")
+
+name="Saurabh Mishra"
+print("My name is:",name)
+
---
+++ Python_2.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+name="Saurabh Mishra"
+print("My name is:",name)
+
+age=32
+print("My age is:",age)
+
+print(type(name))
+
+print(type(age))
+
+str1="TechDBA"
+str2="_Future"
+final_str=str1+str2
+print(final_str)
+print(type(final_str))
